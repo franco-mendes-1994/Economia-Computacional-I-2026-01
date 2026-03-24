377bf7a179c81b6aeec838f019ae032535e9f2ad
franco-mendes-1994 <155776520+franco-mendes-1994@users.noreply.github.com>
2026-03-24 17:06:56 -0300

    Add files via upload

---
+++ Aula 24-03-2026/Imprime 5/Imprime 5/imprime5_010.py
@@ -0,0 +1,26 @@
+"""
+Programa imprime5
+Descrição: Este programa imprime na tela os 5 primeiros números naturais
+Autor: Franco Mendes de Souza
+Data: 24/03/2026
+Versão: 0.1.0
+Notas da versão: Esta versão elimina a alocação desnecessária de memória
+"""
+
+# Alocação de memória
+
+i = 0
+
+# Entrada de Dados
+
+
+
+# Processamento de dados
+
+
+
+# Saída de dados
+
+while i < 5:
+    print(i + 1)
+    i = i + 1
