0761c6c806d25b634e190255251d173e33d6c989
franco-mendes-1994 <155776520+franco-mendes-1994@users.noreply.github.com>
2026-04-12 17:40:43 -0300

    Add files via upload

---
+++ Aula 17-03-2026/soma002.py
@@ -0,0 +1,28 @@
+
+# Programa que soma002
+# Descrição: Este programa lê dois números digitados pelo usuário e calcula a soma deles
+# Autor: Franco Mendes de Souza
+# Versão: 0.0.2
+
+# Alocação de memória
+
+numero_1 = 0
+
+numero_2 = 0
+
+soma = 0
+
+# Entrada de dados
+
+numero_1 = input("Qual a primeira parcela: ")
+
+numero_2 = input("Qual a segunda parcela: ")
+
+
+# Processamento de dados: Cálculo da soma
+
+soma = numero_1 + numero_2
+
+# Saída de dados
+
+print(soma)
