8c84ceedf7f32d8710f54642af73ce6ff986b6c7
franco-mendes-1994 <155776520+franco-mendes-1994@users.noreply.github.com>
2026-04-12 17:42:19 -0300

    Add files via upload

---
+++ Primeira aula/nome_do_programa.py
@@ -0,0 +1,26 @@
+""" nome_do_programa
+
+Descrição: Este programa explica a estrutura básica de um código escrito na linguagem de programação Python
+autor: Franco Mendes de Souza
+Data: 10/03/2026
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+
+
+
+# Entrada de dados
+
+
+
+
+# Processamento de dados
+
+
+
+
+# Saída de dados
+
+
---
+++ Primeira aula/soma.py
@@ -0,0 +1,30 @@
+""" soma
+
+Descrição:O programa lê dois números do programado, soma eles e imprime da tela
+autor: Franco Mendes de Souza
+Data: 10/03/2026
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+numero_1= 2
+
+numero_2 = 3
+
+soma = 0
+
+
+# Entrada de dados
+
+
+
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+
+# Saída de dados
+print(soma)
+
